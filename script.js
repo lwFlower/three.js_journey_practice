48dca4835f252b6ca923a06ce7351491a45d64f4
lwFlower <arina_shevchenko_00@mail.ru>
2026-01-18 21:44:33 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+const canvas = document.querySelector('canvas.webgl');
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const size = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 3;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(size.width, size.height);
+
+renderer.render(scene, camera);

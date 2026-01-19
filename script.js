5d8f2c01283b52b0d93f978a2c231cd079d91639
lwFlower <arina_shevchenko_00@mail.ru>
2026-01-19 16:42:34 +0700

    Lesson 04 - added group and rotation

--- script.js
+++ script.js
@@ -5,9 +5,59 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+
+//box
+const material = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.5});
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+
+//wireframe
+const wireframeGeometry = new THREE.WireframeGeometry(geometry);
+const wireframeMaterial = new THREE.LineBasicMaterial({
+  color: 'white'
+});
+const wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
+// wireframe.material.depthTest = false;
+// wireframe.renderOrder = 1;
+
+//cube object
+const cube = new THREE.Object3D();
+cube.add(mesh)
+cube.add(wireframe);
+//cube.scale.x = 2;
+
+//Rotation
+// cube.rotation.reorder('YXZ');
+// cube.rotation.x = Math.PI * 1.34
+// cube.rotation.y = Math.PI * 0.25;
+
+scene.add(cube);
+
+//Group
+const group = new THREE.Group();
+scene.add(group);
+group.add(cube);
+
+const purpleMaterial = material.clone();
+const purpleCube = new THREE.Mesh(geometry, purpleMaterial);
+purpleCube.material.color.set('purple');
+
+purpleCube.position.x = 1.5;
+group.add(purpleCube);
+
+const blueMaterial = material.clone();
+const blueCube = new THREE.Mesh(geometry, blueMaterial);
+blueCube.material.color.set('blue');
+
+blueCube.position.x = -1.5;
+group.add(blueCube);
+
+group.position.y = -0.5;
+group.rotation.y = Math.PI * 0.25;
+
+const axesHelper = new THREE.AxesHelper(1.5);
+// axesHelper.position.z = 0.5;
+// axesHelper.position.y = 0.5;
+scene.add(axesHelper);
 
 const size = {
   width: 800,
@@ -16,6 +66,15 @@ const size = {
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 3;
+camera.position.y = 1.7;
+//camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), -0.5)
+// const center = new THREE.Vector3()
+//   .addVectors(cube.position, purpleCube.position)
+//   .multiplyScalar(0.5);
+
+// camera.position.set(center.x, 1, 3);
+// camera.lookAt(center);
+camera.lookAt(cube.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({

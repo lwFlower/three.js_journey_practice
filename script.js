5f8a711f5c8729e5997f0c46a538c25fb7f000a9
lwFlower <arina_shevchenko_00@mail.ru>
2026-01-19 18:58:45 +0700

    Lesson -5 - animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const canvas = document.querySelector('canvas.webgl');
 
@@ -52,7 +53,7 @@ blueCube.position.x = -1.5;
 group.add(blueCube);
 
 group.position.y = -0.5;
-group.rotation.y = Math.PI * 0.25;
+//group.rotation.y = Math.PI * 0.25;
 
 const axesHelper = new THREE.AxesHelper(1.5);
 // axesHelper.position.z = 0.5;
@@ -82,4 +83,47 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(size.width, size.height);
 
-renderer.render(scene, camera);
+//rotation calculation
+let direction = 1;
+const speed = 0.01;
+const maxRotation = Math.PI;
+
+//Time test
+//let time = Date.now();
+
+//Clock
+const clock = new THREE.Clock();
+
+//gsap test
+// gsap.to(group.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(group.position, { duration: 1, delay: 2, x: 0 });
+
+//Animation
+const loop = () => {
+
+
+  //Clock
+  const elapsedTime = clock.getElapsedTime();
+
+  //Time test
+  // const currentTime = Date.now();
+  // const deltaTime = currentTime - time;
+  // time = currentTime;
+  // console.log(deltaTime);
+
+  //Update objs
+  //group.rotation.y = speed * direction;
+
+  // if (group.rotation.y >= maxRotation || group.rotation.y <= 0) {
+  //   direction *= -1;
+  // }
+
+  group.rotation.y = Math.sin(elapsedTime * 0.5) * Math.PI;
+  group.rotation.x = Math.sin(elapsedTime * 0.5) * Math.PI;
+
+  //Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(loop);
+}
+loop();

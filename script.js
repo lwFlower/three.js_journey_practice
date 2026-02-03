5ee66b1231d0d59d9f057c940f85d4510449a14f
lwFlower <arina_shevchenko_00@mail.ru>
2026-02-03 14:09:48 +0700

    Lesson 08 - geometries

--- script.js
+++ script.js
@@ -31,8 +31,21 @@ const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
+//BufferGeometry experiment
+// const geometry = new THREE.BufferGeometry();
+
+// const count = 50;
+// const positionsArray = new Float32Array(count * 3 * 3);
+
+// for (let i = 0; i < count * 3 * 3; i++){ 
+//   positionsArray[i] = Math.random() - 0.5;
+// };
+
+// const positionAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionAttribute);
+
 //box
-const material = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.5});
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.5 });
 const mesh = new THREE.Mesh(geometry, material);
 
 //wireframe
